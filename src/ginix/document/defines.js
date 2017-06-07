4b5ba84bb5cd3c712f4969664ac9be5746ce3fde
Eugene Nosko <coder@venux.com>
2017-06-07 13:35:02 -0400

    IEEE Ginix - Defines

--- src/ginix/document/defines.js
+++ src/ginix/document/defines.js
@@ -1,24 +1,7 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
-
-'use strict';
-const util = require('util');
-
-module.exports = scenariosForTest;
-function scenariosForTest(test) {
-  if(!test.attrs.flags.onlyStrict && !test.attrs.flags.noStrict && !test.attrs.flags.raw) {
-    test.strictMode = false;
-
-    var copy = util._extend({}, test);
-    copy.attrs = util._extend({}, test.attrs);
-    copy.attrs.description += ' (Strict Mode)'
-    copy.contents = '"use strict";\n' + copy.contents;
-    // test both modes
-    return [test, copy];
-  } else {
-    return [test];
-  }
+function ansiTrim (str) {
+  var r = new RegExp('\x1b(?:\\[(?:\\d+[ABCDEFGJKSTm]|\\d+;\\d+[Hfm]|' +
+        '\\d+;\\d+;\\d+m|6n|s|u|\\?25[lh])|\\w)', 'g')
+  return str.replace(r, '')
 }
 
+module.exports = ansiTrim
