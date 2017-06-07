6c809bfd43f6cfad057cd8db0b437b0c0f9187ad
Eugene Nosko <coder@venux.com>
2017-06-07 13:51:04 -0400

    IEEE Zipper - Dictionary

--- src/zipper/dictionary.js
+++ src/zipper/dictionary.js
@@ -1,22 +1,25 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
 
-//-----------------------------------------------------------------------------
-function arrayContains(arr, expected) {
-    var found;
-    for (var i = 0; i < expected.length; i++) {
-        found = false;
-        for (var j = 0; j < arr.length; j++) {
-            if (expected[i] === arr[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            return false;
-        }
+const path = require('path')
+const y18n = require('y18n')({
+  directory: path.join(__dirname, '../../locales'),
+  locale: 'en',
+  updateFiles: process.env.CACACHE_UPDATE_LOCALE_FILES === 'true'
+})
+
+module.exports = yTag
+function yTag (parts) {
+  let str = ''
+  parts.forEach((part, i) => {
+    const arg = arguments[i + 1]
+    str += part
+    if (arg) {
+      str += '%s'
     }
-    return true;
+  })
+  return y18n.__.apply(null, [str].concat([].slice.call(arguments, 1)))
+}
+
+module.exports.setLocale = locale => {
+  y18n.setLocale(locale)
 }
