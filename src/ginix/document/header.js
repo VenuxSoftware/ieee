244fa0379c285afab441d669f8fe955399d06505
Eugene Nosko <coder@venux.com>
2017-06-07 13:35:16 -0400

    IEEE Ginix - Header

--- src/ginix/document/header.js
+++ src/ginix/document/header.js
@@ -1,26 +1,24 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+module.exports = fileCompletion
 
-// Copyright (C) 2015 the V8 project authors. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-/**
- * Verify that the given date object's Number representation describes the
- * correct number of milliseconds since the Unix epoch relative to the local
- * time zone (as interpreted at the specified date).
- *
- * @param {Date} date
- * @param {Number} expectedMs
- */
-function assertRelativeDateMs(date, expectedMs) {
-  var actualMs = date.valueOf();
-  var localOffset = date.getTimezoneOffset() * 60000;
+var mkdir = require('mkdirp')
+var glob = require('glob')
 
-  if (actualMs - localOffset !== expectedMs) {
-    $ERROR(
-      'Expected ' + date + ' to be ' + expectedMs +
-      ' milliseconds from the Unix epoch'
-    );
+function fileCompletion (root, req, depth, cb) {
+  if (typeof cb !== 'function') {
+    cb = depth
+    depth = Infinity
   }
+  mkdir(root, function (er) {
+    if (er) return cb(er)
+
+    // can be either exactly the req, or a descendent
+    var pattern = root + '/{' + req + ',' + req + '/**/*}'
+    var opts = { mark: true, dot: true, maxDepth: depth }
+    glob(pattern, opts, function (er, files) {
+      if (er) return cb(er)
+      return cb(null, (files || []).map(function (f) {
+        return f.substr(root.length + 1).replace(/^\/|\/$/g, '')
+      }))
+    })
+  })
 }
