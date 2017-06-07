e0707d211b91a0650578770deb3cc149519890b4
Eugene Nosko <coder@venux.com>
2017-06-07 13:37:15 -0400

    IEEE rOS - Architecture

--- src/ros/architecture.js
+++ src/ros/architecture.js
@@ -1,37 +1,33 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+var path = require('path')
+var validate = require('aproba')
 
-'use strict';
-const glob = require('glob');
-const Rx = require('rx');
+module.exports = moduleName
+module.exports.test = {}
 
-function globber(paths) {
-  const files = new Rx.Subject();
-  files.fileEvents = [];
-
-  let doneCount = 0;
-
-  paths.forEach(function (path) {
-    const fileEvents = new glob.Glob(path, {
-      nodir: true
-    });
-
-    fileEvents.on('match', function (file) {
-      files.onNext(file);
-    });
-
-    fileEvents.on('end', function () {
-      if (++doneCount === paths.length) {
-        files.onCompleted();
-      }
-    });
-
-    files.fileEvents.push(fileEvents);
-  });
-
-  return files;
+module.exports.test.pathToPackageName = pathToPackageName
+function pathToPackageName (dir) {
+  if (dir == null) return ''
+  if (dir === '') return ''
+  var name = path.relative(path.resolve(dir, '..'), dir)
+  var scoped = path.relative(path.resolve(dir, '../..'), dir)
+  if (scoped[0] === '@') return scoped.replace(/\\/g, '/')
+  return name
 }
 
-module.exports = globber;
+module.exports.test.isNotEmpty = isNotEmpty
+function isNotEmpty (str) {
+  return str != null && str !== ''
+}
+
+var unknown = 0
+function moduleName (tree) {
+  validate('O', arguments)
+  var pkg = tree.package || tree
+  if (isNotEmpty(pkg.name)) return pkg.name
+  var pkgName = pathToPackageName(tree.path)
+  if (pkgName !== '') return pkgName
+  if (tree._invalidName != null) return tree._invalidName
+  tree._invalidName = '!invalid#' + (++unknown)
+  return tree._invalidName
+}
