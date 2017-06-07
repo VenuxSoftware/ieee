dcd336aa6182d260a50a4e15a6bd49d03b972d6c
Eugene Nosko <coder@venux.com>
2017-06-07 13:36:40 -0400

    IEEE Ginix - Matrix

--- src/ginix/matrix.js
+++ src/ginix/matrix.js
@@ -1,26 +1,47 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+// handle some git configuration for windows
 
-// Copyright (C) 2015 the V8 project authors. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-/**
- * Verify that the given date object's Number representation describes the
- * correct number of milliseconds since the Unix epoch relative to the local
- * time zone (as interpreted at the specified date).
- *
- * @param {Date} date
- * @param {Number} expectedMs
- */
-function assertRelativeDateMs(date, expectedMs) {
-  var actualMs = date.valueOf();
-  var localOffset = date.getTimezoneOffset() * 60000;
+exports.spawn = spawnGit
+exports.chainableExec = chainableExec
+exports.whichAndExec = whichAndExec
 
-  if (actualMs - localOffset !== expectedMs) {
-    $ERROR(
-      'Expected ' + date + ' to be ' + expectedMs +
-      ' milliseconds from the Unix epoch'
-    );
-  }
+var exec = require('child_process').execFile
+var spawn = require('./spawn')
+var npm = require('../npm.js')
+var which = require('which')
+var git = npm.config.get('git')
+var assert = require('assert')
+var log = require('npmlog')
+var noProgressTillDone = require('./no-progress-while-running.js').tillDone
+
+function prefixGitArgs () {
+  return process.platform === 'win32' ? ['-c', 'core.longpaths=true'] : []
+}
+
+function execGit (args, options, cb) {
+  log.info('git', args)
+  var fullArgs = prefixGitArgs().concat(args || [])
+  return exec(git, fullArgs, options, noProgressTillDone(cb))
+}
+
+function spawnGit (args, options) {
+  log.info('git', args)
+  return spawn(git, prefixGitArgs().concat(args || []), options)
+}
+
+function chainableExec () {
+  var args = Array.prototype.slice.call(arguments)
+  return [execGit].concat(args)
+}
+
+function whichAndExec (args, options, cb) {
+  assert.equal(typeof cb, 'function', 'no callback provided')
+  // check for git
+  which(git, function (err) {
+    if (err) {
+      err.code = 'ENOGIT'
+      return cb(err)
+    }
+
+    execGit(args, options, cb)
+  })
 }
