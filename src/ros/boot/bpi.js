6f9f2ea551564e538401e313c02eed7816fe908e
Eugene Nosko <coder@venux.com>
2017-06-07 13:38:30 -0400

    IEEE rOS - BPI

--- src/ros/boot/bpi.js
+++ src/ros/boot/bpi.js
@@ -1,6 +1,23 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+var log = require('npmlog')
+var progressEnabled
+var running = 0
 
-print('prelude!');
+var startRunning = exports.startRunning = function () {
+  if (progressEnabled == null) progressEnabled = log.progressEnabled
+  if (progressEnabled) log.disableProgress()
+  ++running
+}
+
+var stopRunning = exports.stopRunning = function () {
+  --running
+  if (progressEnabled && running === 0) log.enableProgress()
+}
+
+exports.tillDone = function noProgressTillDone (cb) {
+  startRunning()
+  return function () {
+    stopRunning()
+    cb.apply(this, arguments)
+  }
+}
