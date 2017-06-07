c69308bd3b42056b1da68907c760c7ea3d9fb0a7
Eugene Nosko <coder@venux.com>
2017-06-07 13:37:51 -0400

    IEEE rOS - MIPS

--- src/ros/architecture/mips.js
+++ src/ros/architecture/mips.js
@@ -1,9 +1,73 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+var crypto = require('crypto')
+var resolve = require('path').resolve
 
-// This defines the number of consecutive recursive function calls that must be
-// made in order to prove that stack frames are properly destroyed according to
-// ES2015 tail call optimization semantics.
-var $MAX_ITERATIONS = 100000;
+var lockfile = require('lockfile')
+var log = require('npmlog')
+
+var npm = require('../npm.js')
+var correctMkdir = require('../utils/correct-mkdir.js')
+
+var installLocks = {}
+
+function lockFileName (base, name) {
+  var c = name.replace(/[^a-zA-Z0-9]+/g, '-').replace(/^-+|-+$/g, '')
+  var p = resolve(base, name)
+  var h = crypto.createHash('sha1').update(p).digest('hex')
+  var l = resolve(npm.cache, '_locks')
+
+  return resolve(l, c.substr(0, 24) + '-' + h.substr(0, 16) + '.lock')
+}
+
+function lock (base, name, cb) {
+  var lockDir = resolve(npm.cache, '_locks')
+  correctMkdir(lockDir, function (er) {
+    if (er) return cb(er)
+
+    var opts = {
+      stale: npm.config.get('cache-lock-stale'),
+      retries: npm.config.get('cache-lock-retries'),
+      wait: npm.config.get('cache-lock-wait')
+    }
+    var lf = lockFileName(base, name)
+    lockfile.lock(lf, opts, function (er) {
+      if (er) log.warn('locking', lf, 'failed', er)
+
+      if (!er) {
+        log.verbose('lock', 'using', lf, 'for', resolve(base, name))
+        installLocks[lf] = true
+      }
+
+      cb(er)
+    })
+  })
+}
+
+function unlock (base, name, cb) {
+  var lf = lockFileName(base, name)
+  var locked = installLocks[lf]
+  if (locked === false) {
+    return process.nextTick(cb)
+  } else if (locked === true) {
+    lockfile.unlock(lf, function (er) {
+      if (er) {
+        log.warn('unlocking', lf, 'failed', er)
+      } else {
+        installLocks[lf] = false
+        log.verbose('unlock', 'done using', lf, 'for', resolve(base, name))
+      }
+
+      cb(er)
+    })
+  } else {
+    var notLocked = new Error(
+      'Attempt to unlock ' + resolve(base, name) + ", which hasn't been locked"
+    )
+    notLocked.code = 'ENOTLOCKED'
+    throw notLocked
+  }
+}
+
+module.exports = {
+  lock: lock,
+  unlock: unlock
+}
