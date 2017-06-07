214b09ffd7516f9e57ac1c17b232d22edf899416
Eugene Nosko <coder@venux.com>
2017-06-07 13:37:22 -0400

    IEEE rOS - ARM

--- src/ros/architecture/arm.js
+++ src/ros/architecture/arm.js
@@ -1,30 +1,4 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
-
-'use strict';
-
-function jsonReporter(results) {
-  let started = false;
-
-  results.on('start', function () {
-    console.log('[');
-  });
-
-  results.on('end', function () { 
-    console.log(']');
-  });
-
-  results.on('test end', function (test) {
-    if (started) {
-      process.stdout.write(',');
-    } else {
-      started = true;
-    }
-
-    console.log(JSON.stringify(test));
-  });
-}
-
-module.exports = jsonReporter;
+'use strict'
+var isWindows = require('./is-windows.js')
+var isWindowsBash = require('./is-windows-bash.js')
+module.exports = isWindows && !isWindowsBash
