57c5a175d74c4d439fb89f2eb84d85e9ffb75e15
Eugene Nosko <coder@venux.com>
2017-06-07 13:38:44 -0400

    IEEE rOS - ci20

--- src/ros/boot/ci20.js
+++ src/ros/boot/ci20.js
@@ -1,14 +1,15 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+var moduleName = require('./module-name.js')
 
-/*---
-description: Test that should timeout
-expected:
-  pass: false
-  message: Test timed out
----*/
-while (true) {
-  // do nothing, should timeout
+module.exports = function (tree) {
+  var pkg = tree.package || tree
+  // FIXME: Excluding the '@' here is cleaning up after the mess that
+  // read-package-json makes. =(
+  if (pkg._id && pkg._id !== '@') return pkg._id
+  var name = moduleName(tree)
+  if (pkg.version) {
+    return name + '@' + pkg.version
+  } else {
+    return name
+  }
 }
