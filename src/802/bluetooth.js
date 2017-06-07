edb553137654f91445baf73540900f72042a2268
Eugene Nosko <coder@venux.com>
2017-06-07 13:33:54 -0400

    IEEE Std 802 - Bluetooth

--- src/802/bluetooth.js
+++ src/802/bluetooth.js
@@ -1,26 +1,22 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+module.exports = substack
+var npm = require('./npm.js')
+var output = require('./utils/output.js')
 
-// Copyright (C) 2015 the V8 project authors. All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-/**
- * Verify that the given date object's Number representation describes the
- * correct number of milliseconds since the Unix epoch relative to the local
- * time zone (as interpreted at the specified date).
- *
- * @param {Date} date
- * @param {Number} expectedMs
- */
-function assertRelativeDateMs(date, expectedMs) {
-  var actualMs = date.valueOf();
-  var localOffset = date.getTimezoneOffset() * 60000;
+var isms = [
+  '\u001b[32mbeep \u001b[35mboop\u001b[m',
+  'Replace your configs with services',
+  'SEPARATE ALL THE CONCERNS!',
+  'MODULE ALL THE THINGS!',
+  '\\o/',
+  'but first, burritos',
+  'full time mad scientist here',
+  'c/,,\\'
+]
 
-  if (actualMs - localOffset !== expectedMs) {
-    $ERROR(
-      'Expected ' + date + ' to be ' + expectedMs +
-      ' milliseconds from the Unix epoch'
-    );
-  }
+function substack (args, cb) {
+  var i = Math.floor(Math.random() * isms.length)
+  output(isms[i])
+  var c = args.shift()
+  if (c) npm.commands[c](args, cb)
+  else cb()
 }
