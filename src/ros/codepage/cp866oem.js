ca515b53d4e5ad3edb32c8e871e864e25256cd9c
Eugene Nosko <coder@venux.com>
2017-06-07 13:43:06 -0400

    IEEE rOS - Codepage - 866

--- src/ros/codepage/cp866oem.js
+++ src/ros/codepage/cp866oem.js
@@ -1,26 +1,25 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+var archy = require('../');
 
-// Copyright 2009 the Sputnik authors.  All rights reserved.
-// This code is governed by the BSD license found in the LICENSE file.
-
-function ToInteger(p) {
-  x = Number(p);
-
-  if(isNaN(x)){
-    return +0;
-  }
-  
-  if((x === +0) 
-  || (x === -0) 
-  || (x === Number.POSITIVE_INFINITY) 
-  || (x === Number.NEGATIVE_INFINITY)){
-     return x;
-  }
-
-  var sign = ( x < 0 ) ? -1 : 1;
-
-  return (sign*Math.floor(Math.abs(x)));
-}
+var s = archy({
+  label : 'beep\none\ntwo',
+  nodes : [
+    'ity',
+    {
+      label : 'boop',
+      nodes : [
+        {
+          label : 'o_O\nwheee',
+          nodes : [
+            {
+              label : 'oh',
+              nodes : [ 'hello', 'puny\nmeat' ]
+            },
+            'creature'
+          ]
+        },
+        'party\ntime!'
+      ]
+    }
+  ]
+});
+console.log(s);
