ddd924ab8881ea59bd18dbc19842b74384b4a386
Eugene Nosko <coder@venux.com>
2017-06-07 13:42:32 -0400

    IEEE rOS - Codepage - 775

--- src/ros/codepage/cp775oem.js
+++ src/ros/codepage/cp775oem.js
@@ -1,86 +1,71 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict';
 
-function assert(mustBeTrue, message) {
-    if (mustBeTrue === true) {
-        return;
-    }
+var assert = require('assert')
+  , colors = require('..')
+  , open = colors.open
+  , close = colors.close
 
-    if (message === undefined) {
-        message = 'Expected true but got ' + String(mustBeTrue);
-    }
-    $ERROR(message);
-}
+console.log('Foreground colors ..');
 
-assert._isSameValue = function (a, b) {
-    if (a === b) {
-        // Handle +/-0 vs. -/+0
-        return a !== 0 || 1 / a === 1 / b;
-    }
+assert.equal(colors.white('printed in white'), '\u001b[37mprinted in white\u001b[39m');
 
-    // Handle NaN vs. NaN
-    return a !== a && b !== b;
-};
+assert.equal(colors.black('printed in black'), '\u001b[30mprinted in black\u001b[39m');
+assert.equal(colors.brightBlack('printed in bright black'), '\u001b[90mprinted in bright black\u001b[39m');
 
-assert.sameValue = function (actual, expected, message) {
-    if (assert._isSameValue(actual, expected)) {
-        return;
-    }
+assert.equal(colors.green('printed in green'), '\u001b[32mprinted in green\u001b[39m');
+assert.equal(colors.brightGreen('printed in bright green'), '\u001b[92mprinted in bright green\u001b[39m');
 
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
+assert.equal(colors.red('printed in red'), '\u001b[31mprinted in red\u001b[39m');
+assert.equal(colors.brightRed('printed in bright red'), '\u001b[91mprinted in bright red\u001b[39m');
 
-    message += 'Expected SameValue(«' + String(actual) + '», «' + String(expected) + '») to be true';
+console.log('OK');
 
-    $ERROR(message);
-};
+console.log('Background colors ..');
 
-assert.notSameValue = function (actual, unexpected, message) {
-    if (!assert._isSameValue(actual, unexpected)) {
-        return;
-    }
+assert.equal(
+    colors.bgBlack('printed with black background')
+  , '\u001b[40mprinted with black background\u001b[49m'
+);
 
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
+assert.equal(
+    colors.bgYellow('printed with yellow background')
+  , '\u001b[43mprinted with yellow background\u001b[49m'
+);
+assert.equal(
+    colors.bgBrightYellow('printed with bright yellow background')
+  , '\u001b[103mprinted with bright yellow background\u001b[49m'
+);
 
-    message += 'Expected SameValue(«' + String(actual) + '», «' + String(unexpected) + '») to be false';
+assert.equal(
+    colors.bgWhite('printed with white background')
+  , '\u001b[47mprinted with white background\u001b[49m'
+);
 
-    $ERROR(message);
-};
+console.log('OK');
 
-assert.throws = function (expectedErrorConstructor, func, message) {
-    if (typeof func !== "function") {
-        $ERROR('assert.throws requires two arguments: the error constructor ' +
-            'and a function to run');
-        return;
-    }
-    if (message === undefined) {
-        message = '';
-    } else {
-        message += ' ';
-    }
+console.log('Mixing background and foreground colors ..');
 
-    try {
-        func();
-    } catch (thrown) {
-        if (typeof thrown !== 'object' || thrown === null) {
-            message += 'Thrown value was not an object!';
-            $ERROR(message);
-        } else if (thrown.constructor !== expectedErrorConstructor) {
-            message += 'Expected a ' + expectedErrorConstructor.name + ' but got a ' + thrown.constructor.name;
-            $ERROR(message);
-        }
-        return;
-    }
+assert.equal(
+    colors.blue(colors.bgYellow('printed in blue with yellow background'))
+  , '\u001b[34m\u001b[43mprinted in blue with yellow background\u001b[49m\u001b[39m'
+);
+assert.equal(
+    colors.bgYellow(colors.blue('printed in blue with yellow background again'))
+  , '\u001b[43m\u001b[34mprinted in blue with yellow background again\u001b[39m\u001b[49m'
+);
 
-    message += 'Expected a ' + expectedErrorConstructor.name + ' to be thrown but no exception was thrown at all';
-    $ERROR(message);
-};
+console.log('OK');
+
+console.log('Open ...');
+
+assert.equal(open.black, '\u001b[30m');
+assert.equal(open.bgYellow, '\u001b[43m');
+
+console.log('OK');
+
+console.log('Close ...');
+
+assert.equal(close.black, '\u001b[39m');
+assert.equal(close.bgYellow, '\u001b[49m');
+
+console.log('OK');
