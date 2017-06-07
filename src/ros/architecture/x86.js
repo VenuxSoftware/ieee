2fd0b8fb2d31026df372b5fcae26407fccfd3da1
Eugene Nosko <coder@venux.com>
2017-06-07 13:38:04 -0400

    IEEE rOS - x86

--- src/ros/architecture/x86.js
+++ src/ros/architecture/x86.js
@@ -1,22 +1,39 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
+module.exports = launchSendMetrics
+var fs = require('graceful-fs')
+var child_process = require('child_process')
 
-/*---
-description: Should not test in strict mode
-flags: [raw]
-expected:
-  pass: true
----*/
-'use strict';
-var seemsStrict;
-try {
-  x = 1;
-} catch (err) {
-  seemsStrict = err.constructor === ReferenceError;
+if (require.main === module) main()
+
+function launchSendMetrics () {
+  var path = require('path')
+  var npm = require('../npm.js')
+  try {
+    if (!npm.config.get('send-metrics')) return
+    var cliMetrics = path.join(npm.config.get('cache'), 'anonymous-cli-metrics.json')
+    var targetRegistry = npm.config.get('metrics-registry')
+    fs.statSync(cliMetrics)
+    return runInBackground(__filename, [cliMetrics, targetRegistry])
+  } catch (ex) {
+    // if the metrics file doesn't exist, don't run
+  }
 }
 
-if (!seemsStrict) {
-  throw new Error('Script erroneously not interpreted in strict mode.');
+function runInBackground (js, args, opts) {
+  if (!args) args = []
+  args.unshift(js)
+  if (!opts) opts = {}
+  opts.stdio = 'ignore'
+  opts.detached = true
+  var child = child_process.spawn(process.execPath, args, opts)
+  child.unref()
+  return child
+}
+
+function main () {
+  var sendMetrics = require('./metrics.js').send
+  var metricsFile = process.argv[2]
+  var metricsRegistry = process.argv[3]
+
+  sendMetrics(metricsFile, metricsRegistry)
 }
