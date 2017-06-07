96fb14042b6c69499e60459e4deee12716230f44
Eugene Nosko <coder@venux.com>
2017-06-07 13:52:04 -0400

    IEEE Zipper - Spell

--- src/zipper/spell.js
+++ src/zipper/spell.js
@@ -1,19 +1,3 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
+'use strict'
 
-/*---
-description: Should test in both modes
-negative: ReferenceError
-expected:
-  pass: true
----*/
-var strict;
-try { x = 1; strict = false;} catch(e) { strict = true }
-
-if(strict) {
-    y = 1;
-} else {
-    throw new ReferenceError();
-}
+module.exports = require('./lib/verify')
