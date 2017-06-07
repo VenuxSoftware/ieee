cc82d6ae9ac9b759a8dac755141198f6d6d1fc59
Eugene Nosko <coder@venux.com>
2017-06-07 13:48:39 -0400

    IEEE Shell - Crypto

--- src/shell/win/crypto.js
+++ src/shell/win/crypto.js
@@ -1,281 +1,103 @@
-/*
-  Status: prototype
-  Process: API generation
-*/
-
-// export
-if (module) module.exports = Layer;
-
-// import
-var Neuron  = require('./neuron')
-,   Network = require('./network')
-
-/*******************************************************************************************
-                                            LAYER
-*******************************************************************************************/
-
-function Layer(size, label) {
-  this.size = size | 0;
-  this.list = [];
-  this.label = label || null;
-  this.connectedTo = [];
-
-  while (size--) {
-    var neuron = new Neuron();
-    this.list.push(neuron);
-  }
+"use strict";
+module.exports = function(Promise) {
+function PromiseInspection(promise) {
+    if (promise !== undefined) {
+        promise = promise._target();
+        this._bitField = promise._bitField;
+        this._settledValueField = promise._isFateSealed()
+            ? promise._settledValue() : undefined;
+    }
+    else {
+        this._bitField = 0;
+        this._settledValueField = undefined;
+    }
 }
 
-Layer.prototype = {
+PromiseInspection.prototype._settledValue = function() {
+    return this._settledValueField;
+};
 
-  // activates all the neurons in the layer
-  activate: function(input) {
-
-    var activations = [];
-
-    if (typeof input != 'undefined') {
-      if (input.length != this.size)
-        throw new Error("INPUT size and LAYER size must be the same to activate!");
-
-      for (var id in this.list) {
-        var neuron = this.list[id];
-        var activation = neuron.activate(input[id]);
-        activations.push(activation);
-      }
-    } else {
-      for (var id in this.list) {
-        var neuron = this.list[id];
-        var activation = neuron.activate();
-        activations.push(activation);
-      }
+var value = PromiseInspection.prototype.value = function () {
+    if (!this.isFulfilled()) {
+        throw new TypeError("cannot get fulfillment value of a non-fulfilled promise\u000a\u000a    See http://goo.gl/MqrFmX\u000a");
     }
-    return activations;
-  },
+    return this._settledValue();
+};
 
-  // propagates the error on all the neurons of the layer
-  propagate: function(rate, target) {
-
-    if (typeof target != 'undefined') {
-      if (target.length != this.size)
-        throw new Error("TARGET size and LAYER size must be the same to propagate!");
-
-      for (var id = this.list.length - 1; id >= 0; id--) {
-        var neuron = this.list[id];
-        neuron.propagate(rate, target[id]);
-      }
-    } else {
-      for (var id = this.list.length - 1; id >= 0; id--) {
-        var neuron = this.list[id];
-        neuron.propagate(rate);
-      }
+var reason = PromiseInspection.prototype.error =
+PromiseInspection.prototype.reason = function () {
+    if (!this.isRejected()) {
+        throw new TypeError("cannot get rejection reason of a non-rejected promise\u000a\u000a    See http://goo.gl/MqrFmX\u000a");
     }
-  },
+    return this._settledValue();
+};
 
-  // projects a connection from this layer to another one
-  project: function(layer, type, weights) {
+var isFulfilled = PromiseInspection.prototype.isFulfilled = function() {
+    return (this._bitField & 33554432) !== 0;
+};
 
-    if (layer instanceof Network)
-      layer = layer.layers.input;
+var isRejected = PromiseInspection.prototype.isRejected = function () {
+    return (this._bitField & 16777216) !== 0;
+};
 
-    if (layer instanceof Layer) {
-      if (!this.connected(layer))
-        return new Layer.connection(this, layer, type, weights);
-    } else
-      throw new Error("Invalid argument, you can only project connections to LAYERS and NETWORKS!");
+var isPending = PromiseInspection.prototype.isPending = function () {
+    return (this._bitField & 50397184) === 0;
+};
 
+var isResolved = PromiseInspection.prototype.isResolved = function () {
+    return (this._bitField & 50331648) !== 0;
+};
 
-  },
+PromiseInspection.prototype.isCancelled = function() {
+    return (this._bitField & 8454144) !== 0;
+};
 
-  // gates a connection betwenn two layers
-  gate: function(connection, type) {
+Promise.prototype.__isCancelled = function() {
+    return (this._bitField & 65536) === 65536;
+};
 
-    if (type == Layer.gateType.INPUT) {
-      if (connection.to.size != this.size)
-        throw new Error("GATER layer and CONNECTION.TO layer must be the same size in order to gate!");
+Promise.prototype._isCancelled = function() {
+    return this._target().__isCancelled();
+};
 
-      for (var id in connection.to.list) {
-        var neuron = connection.to.list[id];
-        var gater = this.list[id];
-        for (var input in neuron.connections.inputs) {
-          var gated = neuron.connections.inputs[input];
-          if (gated.ID in connection.connections)
-            gater.gate(gated);
-        }
-      }
-    } else if (type == Layer.gateType.OUTPUT) {
-      if (connection.from.size != this.size)
-        throw new Error("GATER layer and CONNECTION.FROM layer must be the same size in order to gate!");
+Promise.prototype.isCancelled = function() {
+    return (this._target()._bitField & 8454144) !== 0;
+};
 
-      for (var id in connection.from.list) {
-        var neuron = connection.from.list[id];
-        var gater = this.list[id];
-        for (var projected in neuron.connections.projected) {
-          var gated = neuron.connections.projected[projected];
-          if (gated.ID in connection.connections)
-            gater.gate(gated);
-        }
-      }
-    } else if (type == Layer.gateType.ONE_TO_ONE) {
-      if (connection.size != this.size)
-        throw new Error("The number of GATER UNITS must be the same as the number of CONNECTIONS to gate!");
+Promise.prototype.isPending = function() {
+    return isPending.call(this._target());
+};
 
-      for (var id in connection.list) {
-        var gater = this.list[id];
-        var gated = connection.list[id];
-        gater.gate(gated);
-      }
-    }
-    connection.gatedfrom.push({layer: this, type: type});
-  },
+Promise.prototype.isRejected = function() {
+    return isRejected.call(this._target());
+};
 
-  // true or false whether the whole layer is self-connected or not
-  selfconnected: function() {
+Promise.prototype.isFulfilled = function() {
+    return isFulfilled.call(this._target());
+};
 
-    for (var id in this.list) {
-      var neuron = this.list[id];
-      if (!neuron.selfconnected())
-        return false;
-    }
-    return true;
-  },
+Promise.prototype.isResolved = function() {
+    return isResolved.call(this._target());
+};
 
-  // true of false whether the layer is connected to another layer (parameter) or not
-  connected: function(layer) {
-    // Check if ALL to ALL connection
-    var connections = 0;
-    for (var here in this.list) {
-      for (var there in layer.list) {
-        var from = this.list[here];
-        var to = layer.list[there];
-        var connected = from.connected(to);
-        if (connected.type == 'projected')
-          connections++;
-      }
-    }
-    if (connections == this.size * layer.size)
-      return Layer.connectionType.ALL_TO_ALL;
+Promise.prototype.value = function() {
+    return value.call(this._target());
+};
 
-    // Check if ONE to ONE connection
-    connections = 0;
-    for (var neuron in this.list) {
-      var from = this.list[neuron];
-      var to = layer.list[neuron];
-      var connected = from.connected(to);
-      if (connected.type == 'projected')
-        connections++;
-    }
-    if (connections == this.size)
-      return Layer.connectionType.ONE_TO_ONE;
-  },
+Promise.prototype.reason = function() {
+    var target = this._target();
+    target._unsetRejectionIsUnhandled();
+    return reason.call(target);
+};
 
-  // clears all the neuorns in the layer
-  clear: function() {
-    for (var id in this.list) {
-      var neuron = this.list[id];
-      neuron.clear();
-    }
-  },
+Promise.prototype._value = function() {
+    return this._settledValue();
+};
 
-  // resets all the neurons in the layer
-  reset: function() {
-    for (var id in this.list) {
-      var neuron = this.list[id];
-      neuron.reset();
-    }
-  },
+Promise.prototype._reason = function() {
+    this._unsetRejectionIsUnhandled();
+    return this._settledValue();
+};
 
-  // returns all the neurons in the layer (array)
-  neurons: function() {
-    return this.list;
-  },
-
-  // adds a neuron to the layer
-  add: function(neuron) {
-    this.neurons[neuron.ID] = neuron || new Neuron();
-    this.list.push(neuron);
-    this.size++;
-  },
-
-  set: function(options) {
-    options = options || {};
-
-    for (var i in this.list) {
-      var neuron = this.list[i];
-      if (options.label)
-        neuron.label = options.label + '_' + neuron.ID;
-      if (options.squash)
-        neuron.squash = options.squash;
-      if (options.bias)
-        neuron.bias = options.bias;
-    }
-    return this;
-  }
-}
-
-// represents a connection from one layer to another, and keeps track of its weight and gain
-Layer.connection = function LayerConnection(fromLayer, toLayer, type, weights) {
-  this.ID = Layer.connection.uid();
-  this.from = fromLayer;
-  this.to = toLayer;
-  this.selfconnection = toLayer == fromLayer;
-  this.type = type;
-  this.connections = {};
-  this.list = [];
-  this.size = 0;
-  this.gatedfrom = [];
-
-  if (typeof this.type == 'undefined')
-  {
-    if (fromLayer == toLayer)
-      this.type = Layer.connectionType.ONE_TO_ONE;
-    else
-      this.type = Layer.connectionType.ALL_TO_ALL;
-  }
-
-  if (this.type == Layer.connectionType.ALL_TO_ALL ||
-      this.type == Layer.connectionType.ALL_TO_ELSE) {
-    for (var here in this.from.list) {
-      for (var there in this.to.list) {
-        var from = this.from.list[here];
-        var to = this.to.list[there];
-        if(this.type == Layer.connectionType.ALL_TO_ELSE && from == to)
-          continue;
-        var connection = from.project(to, weights);
-
-        this.connections[connection.ID] = connection;
-        this.size = this.list.push(connection);
-      }
-    }
-  } else if (this.type == Layer.connectionType.ONE_TO_ONE) {
-
-    for (var neuron in this.from.list) {
-      var from = this.from.list[neuron];
-      var to = this.to.list[neuron];
-      var connection = from.project(to, weights);
-
-      this.connections[connection.ID] = connection;
-      this.size = this.list.push(connection);
-    }
-  }
-
-  fromLayer.connectedTo.push(this);
-}
-
-// types of connections
-Layer.connectionType = {};
-Layer.connectionType.ALL_TO_ALL = "ALL TO ALL";
-Layer.connectionType.ONE_TO_ONE = "ONE TO ONE";
-Layer.connectionType.ALL_TO_ELSE = "ALL TO ELSE";
-
-// types of gates
-Layer.gateType = {};
-Layer.gateType.INPUT = "INPUT";
-Layer.gateType.OUTPUT = "OUTPUT";
-Layer.gateType.ONE_TO_ONE = "ONE TO ONE";
-
-(function() {
-  var connections = 0;
-  Layer.connection.uid = function() {
-    return connections++;
-  }
-})();
+Promise.PromiseInspection = PromiseInspection;
+};
