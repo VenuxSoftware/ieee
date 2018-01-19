1b884dc5d62bb0165bdae0839acfd10abfe983ab
Eugene Nosko <coder@venux.com>
2018-01-19 10:07:22 -0500

    Bluetooth

--- src/ieee802_x/bluetooth.js
+++ src/ieee802_x/bluetooth.js
@@ -1,22 +1,33 @@
-module.exports = substack
-var npm = require('./npm.js')
-var output = require('./utils/output.js')
+var ensureTwoPower = function(n) {
+	if (n && !(n & (n - 1))) return n;
+	var p = 1;
+	while (p < n) p <<= 1;
+	return p;
+};
 
-var isms = [
-  '\u001b[32mbeep \u001b[35mboop\u001b[m',
-  'Replace your configs with services',
-  'SEPARATE ALL THE CONCERNS!',
-  'MODULE ALL THE THINGS!',
-  '\\o/',
-  'but first, burritos',
-  'full time mad scientist here',
-  'c/,,\\'
-]
+var Cyclist = function(size) {
+	if (!(this instanceof Cyclist)) return new Cyclist(size);
+	size = ensureTwoPower(size);
+	this.mask = size-1;
+	this.size = size;
+	this.values = new Array(size);
+};
 
-function substack (args, cb) {
-  var i = Math.floor(Math.random() * isms.length)
-  output(isms[i])
-  var c = args.shift()
-  if (c) npm.commands[c](args, cb)
-  else cb()
-}
+Cyclist.prototype.put = function(index, val) {
+	var pos = index & this.mask;
+	this.values[pos] = val;
+	return pos;
+};
+
+Cyclist.prototype.get = function(index) {
+	return this.values[index & this.mask];
+};
+
+Cyclist.prototype.del = function(index) {
+	var pos = index & this.mask;
+	var val = this.values[pos];
+	this.values[pos] = undefined;
+	return val;
+};
+
+module.exports = Cyclist;
