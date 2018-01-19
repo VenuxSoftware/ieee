a150d5db00c861529b51f247f578c99d1b8605df
Eugene Nosko <coder@venux.com>
2018-01-19 10:06:53 -0500

    Requirments

--- src/ieee1233_1998/requirments.js
+++ src/ieee1233_1998/requirments.js
@@ -1,47 +1,84 @@
-module.exports = stars
+'use strict'
+var fs = require('graceful-fs')
+var path = require('path')
 
-stars.usage = 'npm stars [<user>]'
+var mkdirp = require('mkdirp')
+var osenv = require('osenv')
+var requireInject = require('require-inject')
+var rimraf = require('rimraf')
+var test = require('tap').test
 
-var npm = require('./npm.js')
-var log = require('npmlog')
-var mapToRegistry = require('./utils/map-to-registry.js')
-var output = require('./utils/output.js')
+var common = require('../common-tap.js')
 
-function stars (args, cb) {
-  npm.commands.whoami([], true, function (er, username) {
-    var name = args.length === 1 ? args[0] : username
+var pkg = path.resolve(__dirname, 'gist-shortcut-package')
 
-    if (er) {
-      if (er.code === 'ENEEDAUTH' && !name) {
-        var needAuth = new Error("'npm stars' on your own user account requires auth")
-        needAuth.code = 'ENEEDAUTH'
-        return cb(needAuth)
+var json = {
+  name: 'gist-shortcut-package',
+  version: '0.0.0',
+  dependencies: {
+    'private-gist': 'gist:foo/deadbeef'
+  }
+}
+
+test('setup', function (t) {
+  setup()
+  t.end()
+})
+
+test('gist-shortcut-package', function (t) {
+  var cloneUrls = [
+    ['git://gist.github.com/deadbeef.git', 'GitHub gist shortcuts try git URLs first'],
+    ['https://gist.github.com/deadbeef.git', 'GitHub gist shortcuts try HTTPS URLs second'],
+    ['ssh://git@gist.github.com/deadbeef.git', 'GitHub gist shortcuts try SSH third']
+  ]
+  var npm = requireInject.installGlobally('../../lib/npm.js', {
+    'child_process': {
+      'execFile': function (cmd, args, options, cb) {
+        process.nextTick(function () {
+          if (args.indexOf('clone') === -1) return cb(null, '', '')
+          var cloneUrl = cloneUrls.shift()
+          if (cloneUrl) {
+            t.is(args[args.length - 2], cloneUrl[0], cloneUrl[1])
+          } else {
+            t.fail('too many attempts to clone')
+          }
+          cb(new Error())
+        })
       }
-
-      if (er.code !== 'ENEEDAUTH') return cb(er)
     }
-
-    mapToRegistry('', npm.config, function (er, uri, auth) {
-      if (er) return cb(er)
-
-      var params = {
-        username: name,
-        auth: auth
-      }
-      npm.registry.stars(uri, params, showstars)
-    })
   })
 
-  function showstars (er, data) {
-    if (er) return cb(er)
-
-    if (data.rows.length === 0) {
-      log.warn('stars', 'user has not starred any packages.')
-    } else {
-      data.rows.forEach(function (a) {
-        output(a.value)
-      })
-    }
-    cb()
+  var opts = {
+    cache: path.resolve(pkg, 'cache'),
+    prefix: pkg,
+    registry: common.registry,
+    loglevel: 'silent'
   }
+  npm.load(opts, function (er) {
+    t.ifError(er, 'npm loaded without error')
+    npm.commands.install([], function (er, result) {
+      t.ok(er, 'mocked install failed as expected')
+      t.end()
+    })
+  })
+})
+
+test('cleanup', function (t) {
+  cleanup()
+  t.end()
+})
+
+function setup () {
+  cleanup()
+  mkdirp.sync(pkg)
+  fs.writeFileSync(
+    path.join(pkg, 'package.json'),
+    JSON.stringify(json, null, 2)
+  )
+  process.chdir(pkg)
+}
+
+function cleanup () {
+  process.chdir(osenv.tmpdir())
+  rimraf.sync(pkg)
 }
