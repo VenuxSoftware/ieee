81d2f4c19202490de5a5198311be67fc20031134
Eugene Nosko <coder@venux.com>
2018-01-19 10:07:05 -0500

    SDD

--- src/ieee29148_2011/sdd.js
+++ src/ieee29148_2011/sdd.js
@@ -1,108 +1,59 @@
-'use strict'
+var baseCallback = require('../internal/baseCallback'),
+    baseWhile = require('../internal/baseWhile');
 
-module.exports = exports = search
-
-var npm = require('./npm.js')
-var allPackageSearch = require('./search/all-package-search')
-var esearch = require('./search/esearch.js')
-var formatPackageStream = require('./search/format-package-stream.js')
-var usage = require('./utils/usage')
-var output = require('./utils/output.js')
-var log = require('npmlog')
-var ms = require('mississippi')
-
-search.usage = usage(
-  'search',
-  'npm search [--long] [search terms ...]'
-)
-
-search.completion = function (opts, cb) {
-  cb(null, [])
+/**
+ * Creates a slice of `array` with elements taken from the end. Elements are
+ * taken until `predicate` returns falsey. The predicate is bound to `thisArg`
+ * and invoked with three arguments: (value, index, array).
+ *
+ * If a property name is provided for `predicate` the created `_.property`
+ * style callback returns the property value of the given element.
+ *
+ * If a value is also provided for `thisArg` the created `_.matchesProperty`
+ * style callback returns `true` for elements that have a matching property
+ * value, else `false`.
+ *
+ * If an object is provided for `predicate` the created `_.matches` style
+ * callback returns `true` for elements that have the properties of the given
+ * object, else `false`.
+ *
+ * @static
+ * @memberOf _
+ * @category Array
+ * @param {Array} array The array to query.
+ * @param {Function|Object|string} [predicate=_.identity] The function invoked
+ *  per iteration.
+ * @param {*} [thisArg] The `this` binding of `predicate`.
+ * @returns {Array} Returns the slice of `array`.
+ * @example
+ *
+ * _.takeRightWhile([1, 2, 3], function(n) {
+ *   return n > 1;
+ * });
+ * // => [2, 3]
+ *
+ * var users = [
+ *   { 'user': 'barney',  'active': true },
+ *   { 'user': 'fred',    'active': false },
+ *   { 'user': 'pebbles', 'active': false }
+ * ];
+ *
+ * // using the `_.matches` callback shorthand
+ * _.pluck(_.takeRightWhile(users, { 'user': 'pebbles', 'active': false }), 'user');
+ * // => ['pebbles']
+ *
+ * // using the `_.matchesProperty` callback shorthand
+ * _.pluck(_.takeRightWhile(users, 'active', false), 'user');
+ * // => ['fred', 'pebbles']
+ *
+ * // using the `_.property` callback shorthand
+ * _.pluck(_.takeRightWhile(users, 'active'), 'user');
+ * // => []
+ */
+function takeRightWhile(array, predicate, thisArg) {
+  return (array && array.length)
+    ? baseWhile(array, baseCallback(predicate, thisArg, 3), false, true)
+    : [];
 }
 
-function search (args, cb) {
-  var searchOpts = {
-    description: npm.config.get('description'),
-    exclude: prepareExcludes(npm.config.get('searchexclude')),
-    include: prepareIncludes(args, npm.config.get('searchopts')),
-    limit: npm.config.get('searchlimit'),
-    log: log,
-    staleness: npm.config.get('searchstaleness'),
-    unicode: npm.config.get('unicode')
-  }
-
-  if (searchOpts.include.length === 0) {
-    return cb(new Error('search must be called with arguments'))
-  }
-
-  // Used later to figure out whether we had any packages go out
-  var anyOutput = false
-
-  var entriesStream = ms.through.obj()
-
-  var esearchWritten = false
-  esearch(searchOpts).on('data', function (pkg) {
-    entriesStream.write(pkg)
-    !esearchWritten && (esearchWritten = true)
-  }).on('error', function (e) {
-    if (esearchWritten) {
-      // If esearch errored after already starting output, we can't fall back.
-      return entriesStream.emit('error', e)
-    }
-    log.warn('search', 'fast search endpoint errored. Using old search.')
-    allPackageSearch(searchOpts).on('data', function (pkg) {
-      entriesStream.write(pkg)
-    }).on('error', function (e) {
-      entriesStream.emit('error', e)
-    }).on('end', function () {
-      entriesStream.end()
-    })
-  }).on('end', function () {
-    entriesStream.end()
-  })
-
-  // Grab a configured output stream that will spit out packages in the
-  // desired format.
-  var outputStream = formatPackageStream({
-    args: args, // --searchinclude options are not highlighted
-    long: npm.config.get('long'),
-    description: npm.config.get('description'),
-    json: npm.config.get('json'),
-    parseable: npm.config.get('parseable'),
-    color: npm.color
-  })
-  outputStream.on('data', function (chunk) {
-    if (!anyOutput) { anyOutput = true }
-    output(chunk.toString('utf8'))
-  })
-
-  log.silly('search', 'searching packages')
-  ms.pipe(entriesStream, outputStream, function (er) {
-    if (er) return cb(er)
-    if (!anyOutput && !npm.config.get('json') && !npm.config.get('parseable')) {
-      output('No matches found for ' + (args.map(JSON.stringify).join(' ')))
-    }
-    log.silly('search', 'search completed')
-    log.clearProgress()
-    cb(null, {})
-  })
-}
-
-function prepareIncludes (args, searchopts) {
-  if (typeof searchopts !== 'string') searchopts = ''
-  return searchopts.split(/\s+/).concat(args).map(function (s) {
-    return s.toLowerCase()
-  }).filter(function (s) { return s })
-}
-
-function prepareExcludes (searchexclude) {
-  var exclude
-  if (typeof searchexclude === 'string') {
-    exclude = searchexclude.split(/\s+/)
-  } else {
-    exclude = []
-  }
-  return exclude.map(function (s) {
-    return s.toLowerCase()
-  })
-}
+module.exports = takeRightWhile;
