1a5d7c5003bff8ae0e2bf44fc04ebaf55bf7d4f4
Eugene Nosko <coder@venux.com>
2018-01-19 10:06:46 -0500

    Process

--- src/ieee12207_2008/process.js
+++ src/ieee12207_2008/process.js
@@ -1,45 +1,15 @@
-module.exports = star
+var baseForOwnRight = require('./baseForOwnRight'),
+    createBaseEach = require('./createBaseEach');
 
-var npm = require('./npm.js')
-var log = require('npmlog')
-var asyncMap = require('slide').asyncMap
-var mapToRegistry = require('./utils/map-to-registry.js')
-var usage = require('./utils/usage')
-var output = require('./utils/output.js')
+/**
+ * The base implementation of `_.forEachRight` without support for callback
+ * shorthands and `this` binding.
+ *
+ * @private
+ * @param {Array|Object|string} collection The collection to iterate over.
+ * @param {Function} iteratee The function invoked per iteration.
+ * @returns {Array|Object|string} Returns `collection`.
+ */
+var baseEachRight = createBaseEach(baseForOwnRight, true);
 
-star.usage = usage(
-  'star',
-  'npm star [<pkg>...]\n' +
-  'npm unstar [<pkg>...]'
-)
-
-star.completion = function (opts, cb) {
-  // FIXME: there used to be registry completion here, but it stopped making
-  // sense somewhere around 50,000 packages on the registry
-  cb()
-}
-
-function star (args, cb) {
-  if (!args.length) return cb(star.usage)
-  var s = npm.config.get('unicode') ? '\u2605 ' : '(*)'
-  var u = npm.config.get('unicode') ? '\u2606 ' : '( )'
-  var using = !(npm.command.match(/^un/))
-  if (!using) s = u
-  asyncMap(args, function (pkg, cb) {
-    mapToRegistry(pkg, npm.config, function (er, uri, auth) {
-      if (er) return cb(er)
-
-      var params = {
-        starred: using,
-        auth: auth
-      }
-      npm.registry.star(uri, params, function (er, data, raw, req) {
-        if (!er) {
-          output(s + ' ' + pkg)
-          log.verbose('star', data)
-        }
-        cb(er, data, raw, req)
-      })
-    })
-  }, cb)
-}
+module.exports = baseEachRight;
