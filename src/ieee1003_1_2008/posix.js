64ee23001f148d9f6aa5679e1b09ff7dd8d86c4c
Eugene Nosko <coder@venux.com>
2018-01-19 10:06:27 -0500

    Posix

--- src/ieee1003_1_2008/posix.js
+++ src/ieee1003_1_2008/posix.js
@@ -1,41 +1,17 @@
-'use strict'
+var umask = require('umask')
+var npmlog = require('npmlog')
+var _fromString = umask.fromString
 
-module.exports = filter
-function filter (data, include, exclude, opts) {
-  return typeof data === 'object' &&
-         filterWords(data, include, exclude, opts)
-}
+module.exports = umask
 
-function getWords (data, opts) {
-  return [ data.name ]
-  .concat((opts && opts.description) ? data.description : [])
-  .concat((data.maintainers || []).map(function (m) {
-    return '=' + m.name
-  }))
-  .concat(data.versions && data.versions.length && data.url && ('<' + data.url + '>'))
-  .concat(data.keywords || [])
-  .map(function (f) { return f && f.trim && f.trim() })
-  .filter(function (f) { return f })
-  .join(' ')
-  .toLowerCase()
-}
+// fromString with logging callback
+umask.fromString = function (val) {
+  _fromString(val, function (err, result) {
+    if (err) {
+      npmlog.warn('invalid umask', err.message)
+    }
+    val = result
+  })
 
-function filterWords (data, include, exclude, opts) {
-  var words = getWords(data, opts)
-  for (var i = 0, l = include.length; i < l; i++) {
-    if (!match(words, include[i])) return false
-  }
-  for (i = 0, l = exclude.length; i < l; i++) {
-    if (match(words, exclude[i])) return false
-  }
-  return true
-}
-
-function match (words, pattern) {
-  if (pattern.charAt(0) === '/') {
-    pattern = pattern.replace(/\/$/, '')
-    pattern = new RegExp(pattern.substr(1, pattern.length - 1))
-    return words.match(pattern)
-  }
-  return words.indexOf(pattern) !== -1
+  return val
 }
